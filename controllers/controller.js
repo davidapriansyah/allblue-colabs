8d98ecd5913f5ce78f7a9c24e2fd482b66409bd5
David <davidapriansyah5@gmail.com>
2024-10-09 21:34:17 +0700

    setup-router-app-views-controller

---
+++ controllers/controller.js
@@ -0,0 +1,113 @@
+const {User,Product,Order,Category,} = require('../models/index.js')
+
+class Controller {
+    static async adminHome (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+
+
+    static async category (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+
+    static async user (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async renderEditUser (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async handleEditUser (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async deleteUser (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async product (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async renderAddProduct (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async handleAddProduct (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async renderEditProduct (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async handleEditProduct (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async deleteProduct (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+    static async order (req, res) {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            res.send(error)
+        }
+    }
+}
+
+module.exports = Controller
